317c4d0cff80f06fe1efa0a66037586af126026b
Stacy Druart <stacy@StacyDruartMSI>
2024-05-22 14:07:29 +0200

    aj

--- assets/js/modules/DelRequest.js
+++ assets/js/modules/DelRequest.js
@@ -1,9 +1,27 @@
-function FunctDel() {
+export function FunctDel() {
     const element = document.getElementById("idEvent");
     element.remove();
   }
 
+export function FunctDel(){
+    fetch('')
+}
+// /api/events/[id]/	
 
 
+//   export function displayEvents() {
+//     fetch('http://localhost:3000/api/events/')
+//         .then(response => {
+//             if (!response.ok) {
+//                 console.log('no found')
+//             }
+//             return response.json()
+//         })
+//         .then(dataEvent => {
+//             creatDomEvent(dataEvent)
+//         })
+//         .catch(error => {
+//             console.error('Erreur:', error)
+//         })
+// }
 
-  
