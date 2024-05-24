0148f39f1d21c8de9ac15cee2359e3678c7f579d
Giovanni_T <giovannitummino.15@gmail.com>
2024-05-24 16:58:07 +0200

    modif

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,9 +1,16 @@
 const toggleDarkMode = () => {
-    const body = document.body;
+    /*const body = document.body;
     body.classList.toggle('dark-theme');
     const section = document.querySelector('section');
     section.classList.toggle('dark-theme');
-    console.log('Dark mode toggled. Current classes on body:', body.className);
-};
+    console.log('Dark mode toggled. Current classes on body:', body.className);*/
+    const body = document.body;
+    body.classList.toggle('dark');
+    body.querySelectorAll('*').forEach(el => { 
+        el.classList.toggle('dark');
+
+    });
+        
+}
 
 export { toggleDarkMode };
